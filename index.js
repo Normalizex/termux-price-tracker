683b5e25b68cc79acdd4d55ec0c8bf343472ed01
Normalizex <104392358+Normalizex@users.noreply.github.com>
2024-09-30 21:29:08 +0300

    init files

---
+++ index.js
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+import axios from 'axios';
+import prompt from 'prompt';
+import BigNumber from 'bignumber.js';
+import { exec } from 'child_process';
+
+const bootstrap = async () => {
+
+};
+
+
+bootstrap();
